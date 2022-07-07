83794e8252cf1b4eece02319222caa7056d5bf3f
DragosPax <dragos1paizan@gmail.com>
2022-07-07 11:32:37 +0300

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  val: string;
+  notificare: string;
+  onNotify(val: string) {
+    console.log(val);
+    this.notificare = val;
+  }
 }
--- src/app/id.component.ts
+++ src/app/id.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CocktailModel } from './cocktail.component';
 import { CocktailService } from './cocktail.service';
 
 @Component({
@@ -6,11 +7,20 @@ import { CocktailService } from './cocktail.service';
   templateUrl: './id.component.html',
 })
 export class GetDrink {
+  @Output() notify: EventEmitter<string> = new EventEmitter<string>();
+
   name = 'something';
+  cocktail: string;
   constructor(private service: CocktailService) {}
 
   getId() {
     const id = document.getElementById('input-one').value;
-    this.service.getCocktail(id).subscribe((resp) => console.log(resp));
+    this.service.getCocktail(id).subscribe((resp) => this.showCocktail(resp));
+    this.notify.emit(this.cocktail);
+  }
+  showCocktail(resp) {
+    this.cocktail = resp.drinks[0].strDrink;
+    console.log(this.cocktail);
+    document.getElementById('cocktail').innerText = this.cocktail;
   }
 }
